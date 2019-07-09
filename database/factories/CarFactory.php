02495d932624d872bacc98a5d8250de541353054
Mona Lisa <gugacns94@gmail.com>
2019-07-09 20:30:13 +0200

    zadatak 1 i 2

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Car;
+
+$factory->define(Car::class, function (Faker $faker) {
+    return [
+        'brand' => $faker->words(1,true),
+        'model' => $faker->words(1,true),
+        'year' => $faker->year(),
+        'maxSpeed' => $faker->numberBetween(50,250),
+        'isAutomatic' => $faker->boolean(),
+        'engine' => $faker->numberBetween(100,250),
+        'numberOfDoors' => $faker->numberBetween(2,5)
+    ];
+});
